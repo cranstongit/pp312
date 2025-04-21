ea6c3c02f73eb236b40dcf987ad1eb34409ea775
cranstongit <alexbludin@gmail.com>
2025-04-22 00:46:24 +0300

    new spring boot web project

---
+++ src/test/java/com/spring/springboot/pp312/Pp312ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.springboot.pp312;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pp312ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
